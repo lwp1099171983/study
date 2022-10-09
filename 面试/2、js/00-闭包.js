3c5299b3436f1ca9fcccd5fd101e81ec8fe42e6f
lwp1099171983 <lwp1099171983@gmail.com>
2022-10-09 13:05:11 +0800

    update

---
+++ 面试/2、js/00-闭包.js
@@ -0,0 +1,27 @@
+function create() {
+    let a = 100;
+    return function () {
+        console.log(a);
+    }
+}
+
+const test = create();
+
+const a = 200;
+
+test(); // 100
+
+
+
+function print(fn) {
+    const b = 200
+    fn()
+}
+
+const b = 100;
+
+function fn () {
+    console.log(b);
+}
+
+print(fn); // 100
