60b96f99fb98badbefa5543567d1016e8e998d40
lwp1099171983 <lwp1099171983@gmail.com>
2022-11-25 12:07:26 +0800

    update

---
+++ zhufeng/es6/02-async/2、generator.js
@@ -0,0 +1,58 @@
+/*
+const genTest = function* () {
+  const y1 = yield 'hello'
+  console.log('y1', y1)
+  const y2 = yield 'world'
+  console.log('y2', y2)
+  return '2333'
+}
+
+const gen = genTest()
+
+const res1 = gen.next()
+// next传参，给上一个yield结果赋值
+const res2 = gen.next('2333')
+const res3 = gen.next('6666')
+
+console.log(res1, res2, res3)
+*/
+
+const fs = require("fs/promises");
+const path = require("path");
+
+function* redFile() {
+  const data1 = yield fs.readFile(path.resolve(__dirname, "a.txt"), "utf8");
+  const data2 = yield fs.readFile(path.resolve(__dirname, data1), "utf8");
+  return data2;
+}
+
+// const it = redFile()
+// const { value, done } = it.next()
+// value.then((data1) => {
+//   console.log('data1', data1)
+//   const { value, done } = it.next(data1)
+//   value.then((data2) => {
+//     const { value, done } = it.next(data2)
+//     console.log('data2', value)
+//   })
+// })
+
+function co(it) {
+  return new Promise((resolve, reject) => {
+    const step = (preYieldVal) => {
+      const { value, done } = it.next(preYieldVal);
+      if (!done) {
+        Promise.resolve(value).then((res) => {
+          step(res);
+        });
+      } else {
+        resolve(value);
+      }
+    };
+    step(it);
+  });
+}
+
+co(redFile()).then((data) => {
+  console.log("data", data);
+});
---
+++ zhufeng/es6/02-async/4、async await.js
@@ -0,0 +1,1 @@
+// async await 其实就是 generator + co 的语法糖
