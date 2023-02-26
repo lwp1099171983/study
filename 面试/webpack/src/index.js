1f8d75ffd735418b5e6ada2c1c6f3fa121a04a79
lwp <1099171983@qq.com>
2023-02-26 10:48:05 +0800

    update

--- 面试/webpack/src/index.js
+++ 面试/webpack/src/index.js
@@ -1,3 +1,27 @@
+import "@/main.css";
+import "@/sass.scss"; // 引入 Sass 文件
+import logo from "../public/A11535439.png";
+
 const a = "Hello webpack";
 console.log(a);
-module.exports = a;
+
+const img = new Image();
+img.src = logo;
+
+document.getElementById("imgBox").appendChild(img);
+
+class Author {
+  name = "ITEM";
+  age = 18;
+  email = "lxp_work@163.com";
+
+  info = () => {
+    return {
+      name: this.name,
+      age: this.age,
+      email: this.email,
+    };
+  };
+}
+
+module.exports = Author;
